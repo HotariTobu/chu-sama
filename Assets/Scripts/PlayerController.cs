3da44d2c6ac21ebfb52b5e8741c088329695125f
HotariTobu <hotari24tools@gmail.com>
2024-05-03 15:14:30 +0900

    fix: player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+using Log = Unity.Logging.Log;
+
 namespace Hotari
 {
     /// <summary>
@@ -10,38 +12,23 @@ namespace Hotari
     /// </summary>
     public class PlayerController : RigidbodyEntity
     {
-        /// <summary>ゲームパッド感度</summary>
-        [SerializeField] float Sensibility = 10.0f;
         /// <summary>移動の係数</summary>
-        [SerializeField] float MoveFactor = 10000.0f;
+        [SerializeField] float MoveFactor = 1000.0f;
+        /// <summary>回転の係数</summary>
+        [SerializeField] float RotateFactor = 4000.0f;
 
         /// <summary>移動の減衰のパラーメータ</summary>
         [SerializeField] float MoveFadeParameter = 0.1f;
         /// <summary>回転の減衰のパラーメータ</summary>
         [SerializeField] float RotateFadeParameter = 0.1f;
 
-        /// <summary>レーザーを発射するためにエネルギーをチャージしているとき、どれくらい移動が鈍くなるか</summary>
-        [SerializeField] float MoveBrakingFactor = 0.1f;
-        /// <summary>レーザーを発射するためにエネルギーをチャージしているとき、どれくらい回転が鈍くなるか</summary>
-        [SerializeField] float RotateBrakingFactor = 0.01f;
-
-        /// <summary>チャージの開始イベント</summary>
-        public Action? StartChargeEvent;
-        /// <summary>チャージの終了イベント</summary>
-        public Action? EndChargeEvent;
-
         /// <summary>Input Action</summary>
         private PlayerInputs? _playerInputs = null;
 
         /// <summary>移動量</summary>
-        private Vector2 _moveInputValue;
-        /// <summary>縦の移動量</summary>
-        private float _verticalMoveValue;
-        /// <summary>カメラの回転量</summary>
-        private Vector2 _lookInputValue;
-
-        /// <summary>動きが鈍くなっているか</summary>
-        private bool _isBraking;
+        private Vector3 _moveInputValue;
+        /// <summary>回転量</summary>
+        private Vector2 _rotateInputValue;
 
         protected override void Start()
         {
@@ -49,17 +36,12 @@ namespace Hotari
 
             _playerInputs = new();
 
-            // _playerInputs.FreeFry.Move.started += OnMove;
-            // _playerInputs.FreeFry.Move.performed += OnMove;
-            // _playerInputs.FreeFry.Move.canceled += OnMove;
-            // _playerInputs.FreeFry.VerticalMove.started += OnVerticalMove;
-            // _playerInputs.FreeFry.VerticalMove.performed += OnVerticalMove;
-            // _playerInputs.FreeFry.VerticalMove.canceled += OnVerticalMove;
-            // _playerInputs.FreeFry.Look.started += OnLook;
-            // _playerInputs.FreeFry.Look.performed += OnLook;
-            // _playerInputs.FreeFry.Look.canceled += OnLook;
-            // _playerInputs.FreeFry.Attack.started += StartCharge;
-            // _playerInputs.FreeFry.Attack.canceled += EndCharge;
+            _playerInputs.FreeFry.Move.started += OnMove;
+            _playerInputs.FreeFry.Move.performed += OnMove;
+            _playerInputs.FreeFry.Move.canceled += OnMove;
+            _playerInputs.FreeFry.Rotate.started += OnRotate;
+            _playerInputs.FreeFry.Rotate.performed += OnRotate;
+            _playerInputs.FreeFry.Rotate.canceled += OnRotate;
 
             _playerInputs.Enable();
 
@@ -86,78 +68,33 @@ namespace Hotari
         /// <param name="context">Moveアクションのコールバック</param>
         private void OnMove(InputAction.CallbackContext context)
         {
-            _moveInputValue = context.ReadValue<Vector2>();
-        }
-
-        /// <summary>
-        /// 縦移動
-        /// </summary>
-        /// <param name="context">Vertical Moveのアクションのコールバック</param>
-        private void OnVerticalMove(InputAction.CallbackContext context)
-        {
-            _verticalMoveValue = context.ReadValue<float>();
+            _moveInputValue = context.ReadValue<Vector3>();
         }
 
         /// <summary>
         /// 回転量の取得
         /// </summary>
-        /// <param name="context">Lookアクションのコールバック</param>
-        private void OnLook(InputAction.CallbackContext context)
+        /// <param name="context">Rotateアクションのコールバック</param>
+        private void OnRotate(InputAction.CallbackContext context)
         {
-            _lookInputValue = context.ReadValue<Vector2>() * Sensibility;
+            _rotateInputValue = context.ReadValue<Vector2>();
         }
 
-        /// <summary>
-        /// チャージ開始
-        /// </summary>
-        private void StartCharge(InputAction.CallbackContext context)
-        {
-            StartChargeEvent?.Invoke();
-            _isBraking = true;
-        }
-
-        /// <summary>
-        /// チャージ終了
-        /// </summary>
-        private void EndCharge(InputAction.CallbackContext context)
-        {
-            EndChargeEvent?.Invoke();
-            _isBraking = false;
-        }
-
-        /// <summary>
-        /// 移動
-        /// </summary>
+        /// <summary>移動</summary>
         private void Move()
         {
-            var inputValue = new Vector3(
-                _moveInputValue.x,
-                _verticalMoveValue,
-                _moveInputValue.y
-            );
-
-            var force = inputValue * MoveFactor * Time.deltaTime;
-            if (_isBraking)
-            {
-                force *= MoveBrakingFactor;
-            }
+            var force = _moveInputValue * MoveFactor * Time.deltaTime;
             _rigidbody.AddRelativeForce(force);
         }
 
-        /// <summary>
-        /// 回転
-        /// </summary>
+        /// <summary>回転</summary>
         private void Rotate()
         {
-            var torque = new Vector3(-_lookInputValue.y, _lookInputValue.x, 0);
-            if (_isBraking)
-            {
-                torque *= RotateBrakingFactor;
-            }
-
+            var torque = new Vector3(-_rotateInputValue.y, _rotateInputValue.x, 0) * RotateFactor * Time.deltaTime;
             _rigidbody.AddRelativeTorque(torque);
         }
 
+        /// <summary>減衰</summary>
         private void Fade()
         {
             _rigidbody.velocity *= Mathf.Pow(MoveFadeParameter, Time.deltaTime);
