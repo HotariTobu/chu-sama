592af9598a318113d745040dfcff9b3d56100da7
qwdfghi <mahiro2400222@gmail.com>
2024-06-07 17:13:39 +0900

    06/07

---
+++ Assets/Scripts/GameManagers/TextBehaviourScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBehaviourScript : MonoBehaviour
+{
+    public Gameobject Texts;
+    private TextMeshProUGUI TextsM;
+    private int cnt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextsM = Texts.GetComponent<TextMeshProUGUI>();
+        cnt = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B)){
+            if(cnt == 1){
+                TextsM.text = "2nd Stage";
+                cnt++;
+            }
+            if(cnt == 2){
+                TextsM.text = "3rd Stage";
+            }
+        }
+    }
+}
