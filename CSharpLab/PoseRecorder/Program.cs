1cc8b06d3de53d9d789c09a65d2fad8fb8dedadb
HotariTobu <hotari24tools@gmail.com>
2024-06-19 08:22:25 +0900

    add: C#lab

---
+++ CSharpLab/PoseRecorder/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Azure.Kinect.Sensor;
+using Microsoft.Azure.Kinect.BodyTracking;
+
+using var device = Device.Open();
+var deviceConfig = new DeviceConfiguration()
+{
+
+};
---
+++ CSharpLab/README.md
@@ -0,0 +1,6 @@
+HOW TO USE .NET on .ipynb
+
+```
+dotnet restore
+dotnet dotnet-interactive jupyter install
+```
